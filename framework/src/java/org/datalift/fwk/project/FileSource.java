783cb402ef64bb609410cdffa63819bc8f7b9012
DEVOS <devos_h@RV-PC-UBUNTU.(none)>
2011-06-21 11:45:31 +0200

    interfaces modifications in fwk modified projectManager build

--- framework/src/java/org/datalift/fwk/project/FileSource.java
+++ framework/src/java/org/datalift/fwk/project/FileSource.java
@@ -10,6 +10,7 @@ import java.net.URI;
 public interface FileSource<T> extends Source, Iterable<T>
 {
     public String getMimeType();
+    public void setMimeType(String mimeType);
     public String getFilePath();
     public InputStream getInputStream() throws IOException;
     public void init(File docRoot, URI baseUri) throws IOException;
