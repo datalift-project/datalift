76f7dc7d97ab786b60e54ceb5c7f5566dac483d4
Laurent Bihanic <laurent.bihanic@atosorigin.com>
2011-06-24 13:52:54 +0200

    Changed CSV to RDF mapping: new named graph and subject URIs, added support for basic literal type detection (double and decimal values)

--- simple-convert/src/java/org/datalift/converter/CsvConverter.java
+++ simple-convert/src/java/org/datalift/converter/CsvConverter.java
@@ -1,45 +1,85 @@
 package org.datalift.converter;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
 
+import java.io.File;
+import java.net.URI;
+
+import javax.persistence.EntityNotFoundException;
 import javax.ws.rs.GET;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
+
+import org.openrdf.model.Literal;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.repository.RepositoryConnection;
 
 import org.datalift.fwk.BaseModule;
 import org.datalift.fwk.Configuration;
-import org.datalift.fwk.log.Logger;
 import org.datalift.fwk.project.CsvSource;
 import org.datalift.fwk.project.Project;
 import org.datalift.fwk.project.ProjectManager;
 import org.datalift.fwk.project.ProjectModule;
 import org.datalift.fwk.project.Source;
-import org.datalift.fwk.project.TransformedRdfSource;
 import org.datalift.fwk.rdf.Repository;
-import org.openrdf.model.Statement;
-import org.openrdf.model.ValueFactory;
-import org.openrdf.repository.RepositoryConnection;
+import org.datalift.fwk.util.StringUtils;
+
 
 public class CsvConverter extends BaseModule implements ProjectModule
 {
-	private final static String MODULE_NAME = "csvconverter";
+    //-------------------------------------------------------------------------
+    // Constants
+    //-------------------------------------------------------------------------
 
-	private ProjectManager	projectManager = null;
+    private final static String MODULE_NAME = "csvconverter";
 
-	private File	storage = null;
+    //-------------------------------------------------------------------------
+    // Instance members
+    //-------------------------------------------------------------------------
 
-	private Repository internal = null;
+    private ProjectManager projectManager = null;
+    private File storage = null;
+    private Repository internal = null;
 
-	private Logger log = Logger.getLogger();
+    //-------------------------------------------------------------------------
+    // Constructors
+    //-------------------------------------------------------------------------
 
     public CsvConverter() {
     	super(MODULE_NAME, true);
     }
 
+    //-------------------------------------------------------------------------
+    // Module contract support
+    //-------------------------------------------------------------------------
+
+    /** {@inheritDoc} */
+    @Override
+    public void init(Configuration configuration) {
+        super.init(configuration);
+
+        this.storage = configuration.getPublicStorage();
+        this.internal = configuration.getInternalRepository();
+
+        this.projectManager = configuration.getBean(ProjectManager.class);
+        if (this.projectManager == null) {
+            throw new RuntimeException("Could not retrieve Project Manager");
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    // ProjectModule contract support
+    //-------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     * @return <code>true</code> if the project contains at least one
+     *         source of type {@link CsvSource}.
+     */
     @Override
     public URI canHandle(Project p) {
         boolean hasCsvSource = false;
@@ -61,95 +101,120 @@ public class CsvConverter extends BaseModule implements ProjectModule
         return projectPage;
     }
 
+    //-------------------------------------------------------------------------
+    // Web services
+    //-------------------------------------------------------------------------
+
     @GET
-    public Object getIndexPage(@QueryParam("project") String projectId,
-    		                   @Context UriInfo uriInfo){
-    	if (projectId != null) {
-    		Project p;
-    		try {
-    			p = this.projectManager.findProject(new URL(projectId).toURI());
-    		} catch (Exception e) {
-    			// should never append
-    			throw new RuntimeException("Could not find project with URI " + projectId, e);
-    		}
-    		CsvSource src = null;
-    		for (Source s : p.getSources()) {
-    			if (s instanceof CsvSource) {
-    				src = (CsvSource)s;
-    				try {
-    					src.init(storage, uriInfo.getBaseUri());
-    					break;
-    				}
-    				catch (IOException e) {
-    					throw new RuntimeException("Could not initialize Source");
-    				}
-    			}
-    		}
-    		if (src != null){
-	    		try {
-	    			URI transformedUri = new URL(src.getUri() + "-rdf" + p.getSources().size()).toURI();
-	    			this.convert(src, this.internal, transformedUri);
-	    			TransformedRdfSource transformedSrc = this.projectManager.newTransformedRdfSource(
-	    					transformedUri, src.getTitle() + "-rdf" + p.getSources().size(),
-	    					transformedUri);
-	    			p.addSource(transformedSrc);
-	    			this.projectManager.saveProject(p);
-	    		}
-	    		catch (Exception e) {
-	    			log.debug("Error while persisting project {}", p.getTitle());
-	    		}
-    		}
-        	return "CSV Conversion done for project " + projectId;
-    	}
-    	return "Converter index page";
+    public String getIndexPage(@QueryParam("project") URI projectId,
+                               @Context UriInfo uriInfo)
+                                                throws WebApplicationException {
+        CsvSource src = null;
+        Project p = this.projectManager.findProject(projectId);
+        if (p != null) {
+            for (Source s : p.getSources()) {
+                if (s instanceof CsvSource) {
+                    src = (CsvSource)s;
+                    // Continue to get last CSV source in project...
+                }
+            }
+        }
+        if (src == null) {
+            throw new EntityNotFoundException("No CSV source found");
+        }
+        try {
+            src.init(storage, uriInfo.getBaseUri());
+
+            String srcName = " (RDF #" + p.getSources().size() + ')';
+            URI targetGraph = new URI(src.getUri()
+                                      + '/' + StringUtils.urlify(srcName));
+            this.convert(src, this.internal, targetGraph);
+            p.addSource(this.projectManager.newTransformedRdfSource(
+                            targetGraph, src.getTitle() + srcName, targetGraph));
+            this.projectManager.saveProject(p);
+        }
+        catch (Exception e) {
+            throw new WebApplicationException(
+                        Response.status(Status.INTERNAL_SERVER_ERROR)
+                                .entity(e.getLocalizedMessage())
+                                .type(MediaType.TEXT_PLAIN)
+                                .build());
+        }
+        return "CSV to RDF conversion completed for source " + src.getTitle();
     }
 
-    public void convert(CsvSource src, Repository target, URI namedGraph) {
-    	int	i = 0;
+    private void convert(CsvSource src, Repository target, URI targetGraph) {
         final RepositoryConnection cnx = target.newConnection();
         try {
             final ValueFactory valueFactory = cnx.getValueFactory();
             // Clear target named graph, if any.
-            org.openrdf.model.URI u = null;
-           if (namedGraph != null) {
-        	   u = valueFactory.createURI(namedGraph.toString());
-        	   cnx.clear(u);
-           }
-           // Prevent transaction commit for each triple inserted.
-           cnx.setAutoCommit(false);
-           // Load triples
-           for (String[] line : src) {
-        	   String subject = namedGraph + "/row" + i; // + "#_";
-        	   for (int j = 0; j < line.length && j < src.getColumnsHeader().size(); j++) {
-        		   String predicate = namedGraph + "/column" + src.getColumnsHeader().get(j);
-        		   Statement stmt = valueFactory.createStatement(
-        				   valueFactory.createURI(subject),
-        				   valueFactory.createURI(predicate),
-        				   valueFactory.createLiteral(line[j]));
-        		   cnx.add(stmt, valueFactory.createURI(namedGraph.toString()));
-        	   }
-    		   i++;
-           }
-           cnx.commit();
+            org.openrdf.model.URI ctx = null;
+            if (targetGraph != null) {
+                ctx = valueFactory.createURI(targetGraph.toString());
+                cnx.clear(ctx);
+            }
+            String baseUri = (targetGraph != null)?
+                                            targetGraph.toString() + '/': "";
+            // Prevent transaction commit for each triple inserted.
+            cnx.setAutoCommit(false);
+            // Build predicates URIs.
+            int max = src.getColumnsHeader().size();
+            org.openrdf.model.URI[] predicates = new org.openrdf.model.URI[max];
+            int i = 0;
+            for (String s : src.getColumnsHeader()) {
+                predicates[i++] = valueFactory.createURI(
+                                            baseUri + StringUtils.urlify(s));
+            }
+            // Load triples
+            i = 0;
+            for (String[] line : src) {
+                String subject = baseUri + i; // + "#_";
+                max = Math.min(line.length, max);
+                for (int j=0; j<max; j++) {
+                    String v = line[j];
+                    if (StringUtils.isSet(v)) {
+                        cnx.add(valueFactory.createStatement(
+                                        valueFactory.createURI(subject),
+                                        predicates[j],
+                                        this.mapValue(v, valueFactory)),
+                                ctx);
+                    }
+                    // Else: ignore cell.
+                }
+                i++;
+            }
+            cnx.commit();
         }
         catch (Exception e) {
-    		throw new RuntimeException("Could not convert CsvSource into RDF triples");
+            throw new RuntimeException("CSV to RDF conversion failed", e);
     	}
         finally {
             try { cnx.close(); } catch (Exception e) { /* Ignore */ }
         }
     }
 
-	@Override
-	public void init(Configuration configuration) {
-		super.init(configuration);
-		ProjectManager mgr = configuration.getBean(ProjectManager.class);
-		if (mgr != null) {
-			this.projectManager = mgr;
-			this.storage = configuration.getPublicStorage();
-			this.internal = configuration.getInternalRepository();
-		}
-		else
-			throw new RuntimeException("Could not retrieve Project Manager");
-	}
+    private Literal mapValue(String s, ValueFactory valueFactory) {
+        Literal v = null;
+        s = s.trim();
+        if ((s.indexOf('.') != -1) || (s.indexOf(',') != -1)) {
+            // Try double.
+            try {
+                v = valueFactory.createLiteral(
+                                    Double.parseDouble(s.replace(',', '.')));
+            }
+            catch (Exception e) { /* Ignore... */ }
+        }
+        if (v == null) {
+            // Try integer.
+            try {
+                v = valueFactory.createLiteral(Long.parseLong(s));
+            }
+            catch (Exception e) { /* Ignore... */ }
+        }
+        if (v == null) {
+            // Assume string literal.
+            v = valueFactory.createLiteral(s);
+        }
+        return v;
+    }
 }
