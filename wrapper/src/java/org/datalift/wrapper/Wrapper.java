a3d1314ad4de1eb6966765ec64c15c3088d7e64f
Laurent Bihanic <laurent.bihanic@atos.net>
2012-03-08 20:44:41 +0100

    Javadoc update.

--- wrapper/src/java/org/datalift/wrapper/Wrapper.java
+++ wrapper/src/java/org/datalift/wrapper/Wrapper.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright / Copr. 2010-2013 Atos - Public Sector France -
+ * BS & Innovation for the DataLift project,
+ * Contributor(s) : L. Bihanic, H. Devos, O. Ventura, M. Chetima
+ *
+ * Contact: dlfr-datalift@atos.net
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty and the software's author, the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading, using, modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean that it is complicated to manipulate, and that also
+ * therefore means that it is reserved for developers and experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and, more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
 
 package org.datalift.wrapper;
 
@@ -18,6 +51,17 @@ import com.centerkey.utils.BareBonesBrowserLaunch;
 import static org.datalift.wrapper.OsType.*;
 
 
+/**
+ * A wrapper to run DataLift in standalone mode.
+ * <p>
+ * This wrapper starts a Jetty servlet container and deploys the
+ * DataLift web application well as the Sesame RDF engine.</p>
+ * <p>
+ * This wrapper has no dependency on the DataLift framework,
+ * application or third-party libraries.</p>
+ *
+ * @author lbihanic
+ */
 public final class Wrapper
 {
     /** The default HTTP port for the DataLift standalone server. */
@@ -31,6 +75,7 @@ public final class Wrapper
     /** The Sesame repository directory system property variable. */
     public final static String SESAME_HOME =
                                         "info.aduna.platform.appdata.basedir";
+
     private final static String SESAME_REPOSITORIES_DIR = "repositories";
 
     private final static String MAC_DATALIFT_NAME = "DataLift";
