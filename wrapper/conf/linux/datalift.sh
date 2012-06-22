7671e3553ce10ef0962a9463cba91158d161258c
Laurent Bihanic <laurent.bihanic@atos.net>
2012-06-22 16:10:52 +0200

    Simpler Linux start-up script

--- wrapper/conf/linux/datalift.sh
+++ wrapper/conf/linux/datalift.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 #
-curDir=`pwd`
 runDir=`dirname $0`
-cd "$runDir"
 #
-# Add all wrapper JARs to classpath.
-for i in bin/*.jar
-do
-  CLASSPATH="${CLASSPATH}:${i}"
-done
-export CLASSPATH
-#
-# Add native libraries directories to JVM library path,
-# in case some modules rely on native libraries.
-JAVA_OPTS="${JAVA_OPTS} -Djava.library.path=bin/lib:$HOME/.datalift/lib"
-#
-# Start wrapper.
-java ${JAVA_OPTS} org.datalift.wrapper.Wrapper
-#
-cd "$curDir"
+# Start the wrapper, setting JVM library path in case some modules
+# rely on native libraries.
+java -Djava.library.path="${runDir}/bin/lib:$HOME/.datalift/lib" \
+     -jar "${runDir}/bin/datalift-wrapper.jar" "${runDir}"
 #
