83f4de348ea2d20b382c07c2e8565b50d7c3b1af
Thibaud Colas <thibaud.colas@etud.univ-montp2.fr>
2012-07-10 12:26:20 +0200

    Modified #i18n rendering to have parameters

--- core/src/java/org/datalift/core/velocity/i18n/I18nDirective.java
+++ core/src/java/org/datalift/core/velocity/i18n/I18nDirective.java
@@ -37,6 +37,7 @@ package org.datalift.core.velocity.i18n;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.text.MessageFormat;
 
 import org.apache.velocity.context.InternalContextAdapter;
 import org.apache.velocity.exception.MethodInvocationException;
@@ -85,25 +86,53 @@ public class I18nDirective extends Directive
         return LINE;
     }
 
-    /** {@inheritDoc} */
-    @Override
-    public boolean render(InternalContextAdapter context, Writer writer,
-                          Node node) 
-                        throws IOException, ResourceNotFoundException,
-                               ParseErrorException, MethodInvocationException {
-        String key = String.valueOf(node.jjtGetChild(0).value(context));
-        // Get bundles from context.
-        BundleList b = (BundleList)(context.get(BundleList.KEY));
-        if (b != null) {
-            // Get value from bundles
-            writer.write(b.getValue(key));
-            return true;
-        }
-        else {
-            log.warn("{}: Failed to resolved key \"{}\"" +
-                     ": no bundle defined in template {}",
-                     this.getName(), key, node.getTemplateName());
-            return false;
-        }
-    }
+    /**
+	* {@inheritDoc}
+	* @return True if the given key exists for our context, false otherwise.
+	*/
+	@Override
+	public boolean render(InternalContextAdapter context, Writer writer,
+	Node node)
+	throws IOException, ResourceNotFoundException,
+	ParseErrorException, MethodInvocationException {
+		// Key is the first child of our node for the given context.
+		String key = String.valueOf(node.jjtGetChild(0).value(context));
+		BundleList b = (BundleList)(context.get(BundleList.KEY));
+		if (b != null) {
+			// Get value from the bundle
+			String msg = b.getValue(key);
+			// Number of childs / params for our node.
+			int params = node.jjtGetNumChildren();
+			
+			// Stronger: indexOf('{') != -1 && indexOf('{') < indexOf('}')
+			// Still possible to use { & } if the message has no parameter.
+			if ((params > 1) && (msg.indexOf('{') != -1)) {
+				
+				Object[] args = new Object[params - 1];
+				//for (int i=0; i<params-1; i++) {
+					// args[i] = node.jjtGetChild(i+1).value(context);
+					//}
+					for (int i=1; i<params; i++) {
+						args[i-1] = node.jjtGetChild(i).value(context);
+					}
+					// Replaces all of the {vars} with their {args}.
+					// Call syntax -> #i18n('project.label' ${it.title})
+					// Properties syntax : project.label = {0} Project
+					msg = MessageFormat.format(msg, args);
+
+					if (log.isDebugEnabled()) {
+						log.debug("#i18n render - args inserted for " + key);
+					}
+				}
+				// Write value where the key was.
+				writer.write(msg);
+				return true;
+			}
+			else {
+				log.warn("{}: Failed to resolved key \"{}\"" +
+					": no bundle defined in template {}",
+					this.getName(), key, node.getTemplateName());
+				return false;
+			}
+		}
 }
