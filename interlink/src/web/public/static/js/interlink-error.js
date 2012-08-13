c837537c882c8a56466980858bbf1ae3aba72c08
Thibaud Colas <thibaud.colas@etud.univ-montp2.fr>
2012-08-14 01:29:35 +0200

    New interlink module first CVS version
    A new interlinking module built around Silk. Main functionalities are: Upload and launch external Silk configurations. Create your own configuration from scratch and either execute or download it.

---
+++ interlink/src/web/public/static/js/interlink-error.js
@@ -0,0 +1,15 @@
+/*
+ * An interface for a {@link ProjectModule project module} that uses Silk to
+ * generate links between two datasets.
+ * Autocompletion using jQuery, custom-made form validation.
+ * 
+ * @author tcolas
+ */
+
+$(document).ready(function() {
+
+	$("#go_back").button();
+	$("#goto_workspace").button();
+	$(".multiple-choices").buttonset();
+	
+});
