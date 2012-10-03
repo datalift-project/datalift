637a0e87a5c21b2dff44a89bf3fbb165bec7d705
Thibaud Colas <thibaudcolas@gmail.com>
2012-10-04 00:22:36 +0200

    New help items
    New help items :
    - global help and description of Silk and its module
    - link to the Silk's official website
    - help item for every field

--- interlink/src/web/public/static/js/interlink-form.js
+++ interlink/src/web/public/static/js/interlink-form.js
@@ -6,8 +6,118 @@
  * @author tcolas
  */
 
+(function($) {
+	$.widget("ui.combobox", {
+		_create: function() {
+			var input,
+				self = this,
+				select = this.element.hide(),
+				selected = select.children(":selected"),
+				value = selected.val() ? selected.text() : "",
+				wrapper = this.wrapper = $( "<span>")
+					.addClass("ui-combobox")
+					.css("width","100%")
+					.insertAfter(select);
+
+			input = $("<input>")
+				.appendTo(wrapper)
+				.val(value)
+				.attr("name", select.attr("name"))
+				.attr("id", select.attr("id"))
+				.attr("tabindex", select.attr("tabindex"))
+				.attr("placeholder", select.children(":first-child").val())
+				.addClass("ui-state-default ui-combobox-input")
+				.autocomplete({
+					delay: 0,
+					minLength: 0,
+					source: function(request, response) {
+						var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+						response(select.children("option").map(function() {
+							var text = $(this).text();
+							if (this.value && (!request.term || matcher.test(text)))
+								return {
+									label: text.replace(
+										new RegExp(
+											"(?![^&;]+;)(?!<[^<>]*)(" +
+											$.ui.autocomplete.escapeRegex(request.term) +
+											")(?![^<>]*>)(?![^&;]+;)", "gi"
+										), "<strong>$1</strong>"),
+									value: text,
+									option: this
+								};
+						}));
+					},
+					select: function(event, ui) {
+						ui.item.option.selected = true;
+						self._trigger("selected", event, {
+							item: ui.item.option
+						});
+					},
+					change: function(event, ui) {
+						if (!ui.item) {
+							var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex($(this).val()) + "$", "i"),
+								valid = false;
+							select.children("option").each(function() {
+								if ($(this).text().match(matcher)) {
+									this.selected = valid = true;
+									return false;
+								}
+							});
+						}
+					}
+				})
+				.addClass("ui-widget ui-widget-content ui-corner-left")
+				.css({
+					background : "none white",
+					color : "#333",
+					"font-weight" : "normal",
+					});
+
+			input.data("autocomplete")._renderItem = function(ul, item) {
+				return $("<li></li>")
+					.data("item.autocomplete", item)
+					.append("<a>" + item.label + "</a>")
+					.appendTo(ul);
+			};
+
+			$("<a>")
+				.attr("tabIndex", -1)
+				.attr("title", "Show All Items")
+				.appendTo(wrapper)
+				.css({background : "#fff", "font-size": "80%"})
+				.button({ label : '&#9660;' })
+				.removeClass("ui-corner-all")
+				.addClass("ui-corner-right ui-combobox-toggle")
+				.click(function() {
+					// close if already visible
+					if (input.autocomplete("widget").is(":visible")) {
+						input.autocomplete("close");
+						return;
+					}
+
+					// work around a bug (likely same cause as #5265)
+					$(this).blur();
+
+					// pass empty string as value to search for, displaying all results
+					input.autocomplete("search", "");
+					input.focus();
+
+					select.attr("name", "");
+					select.attr("id", "");
+					select.attr("tabindex", -10)
+				});
+		},
+
+		destroy: function() {
+			this.wrapper.remove();
+			this.element.show();
+			$.Widget.prototype.destroy.call(this);
+		}
+	});
+})(jQuery);
+
 $(document).ready(function() {
-	$("#file-submit, #form-submit, .cancel-button").button();
+	$("#file-submit, #form-submit, .cancel-button, #form-help").button();
 	$(".multiple-choices").buttonset();
 	$('.add-compare-js, .remove-compare-js').button();
 
@@ -120,6 +230,12 @@ $(document).ready(function() {
 		else {
 			$(".weight-js").show();
 		}
+	});
+
+	/* Displays the help fields. */
+	$("#form-help").click(function (event) {
+		$('.help-js').slideToggle(50);
+		event.preventDefault();
 	});
 
 	$("#script-form").submit(function(){
