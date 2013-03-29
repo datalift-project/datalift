d64adf37c630026dc05ad5814e0eeceef1f25931
Laurent Bihanic <laurent.bihanic@atos.net>
2013-03-29 11:57:28 +0100

    Straighten loading of policy files in S4AC. Improved error messages. Fixed author list in S4acAccessController.

--- s4ac/src/java/org/datalift/s4ac/services/S4acAccessController.java
+++ s4ac/src/java/org/datalift/s4ac/services/S4acAccessController.java
@@ -1,42 +1,9 @@
-/*
- * Copyright / Copr. 2010-2013 Atos - Public Sector France -
- * BS & Innovation for the DataLift project,
- * Contributor(s) : L. Bihanic, H. Devos, O. Ventura, M. Chetima
- *
- * Contact: dlfr-datalift@atos.net
- *
- * This software is governed by the CeCILL license under French law and
- * abiding by the rules of distribution of free software. You can use,
- * modify and/or redistribute the software under the terms of the CeCILL
- * license as circulated by CEA, CNRS and INRIA at the following URL
- * "http://www.cecill.info".
- *
- * As a counterpart to the access to the source code and rights to copy,
- * modify and redistribute granted by the license, users are provided only
- * with a limited warranty and the software's author, the holder of the
- * economic rights, and the successive licensors have only limited
- * liability.
- *
- * In this respect, the user's attention is drawn to the risks associated
- * with loading, using, modifying and/or developing or reproducing the
- * software by the user in light of its specific status of free software,
- * that may mean that it is complicated to manipulate, and that also
- * therefore means that it is reserved for developers and experienced
- * professionals having in-depth computer knowledge. Users are therefore
- * encouraged to load and test the software's suitability as regards their
- * requirements in conditions enabling the security of their systems and/or
- * data to be ensured and, more generally, to use and operate it in the
- * same conditions as regards security.
- *
- * The fact that you are presently reading this means that you have had
- * knowledge of the CeCILL license and that you accept its terms.
- */
-
 package org.datalift.s4ac.services;
 
 import java.io.File;
 import java.text.MessageFormat;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -80,7 +47,8 @@ import static org.datalift.s4ac.utils.QueryType.*;
  * Security for Access Control)</a> vocabulary for specifying the
  * access control policies.
  *
- * @author lbihanic
+ * @author Serena Villata (INRIA)
+ * @author Laurent Bihanic (Atos)
  */
 public class S4acAccessController extends BaseModule
                                   implements AccessController
@@ -182,10 +150,10 @@ public class S4acAccessController extends BaseModule
             }
         }
         this.userContext = new MessageFormat(ctx);
+        // Get the policy files to populate the security repository from.
+        String policyFiles = configuration.getProperty(POLICY_FILES);
         // Connect to or initialize security repository.
         try {
-            // Get the policy files to populate the security repository from.
-            String policyFiles = configuration.getProperty(POLICY_FILES);
             // Load access control restrictions from security RDF store.
             String securityRepositoryUri = configuration.getProperty(
                                                     SECURITY_REPOSITORY_URI);
@@ -204,24 +172,17 @@ public class S4acAccessController extends BaseModule
                 }
             }
             else if (! isBlank(policyFiles)) {
-                // Create an in-memory repository.
+                // Create an in-memory repository to load the access control
+                // policies defined in local RDF files.
                 this.securityRepository = configuration.newRepository(
                             ANON_SECURITY_REPOSITORY_URI, "sail:///", false);
             }
             // Else: No security repository!
-
-            // Load access control policies in security repository.
-            if ((this.securityRepository != null) && (! isBlank(policyFiles))) {
-                for (String f : policyFiles.split("\\s*,\\s*")) {
-                    log.debug("Loading policy file {}...", f);
-                    RdfUtils.upload(new File(f), this.securityRepository, null);
-                }
-            }
         }
         catch (Exception e) {
             throw new TechnicalException("security.repository.acces.error", e);
         }
-
+        // Load access control policies from security repository.
         if (this.securityRepository != null) {
             // Get the name of the repository against which resolving
             // access control policy ASK queries.
@@ -238,6 +199,11 @@ public class S4acAccessController extends BaseModule
                     log.info("Resolving access policy ASK queries against RDF store \"{}\"",
                              this.userRepository.name);
                 }
+            }
+            // Load access control policy files (if any) in security repository.
+            if (! isBlank(policyFiles)) {
+                this.loadPolicyFiles(
+                                Arrays.asList(policyFiles.split("\\s*,\\s*")));
             }
             // Extract access control policies.
             RepositoryConnection cnx = null;
@@ -527,6 +493,28 @@ public class S4acAccessController extends BaseModule
            throw new TechnicalException(e);
        }
        return graphs;
+    }
+
+    private void loadPolicyFiles(Collection<String> policyFiles) {
+        for (String f : policyFiles) {
+            log.debug("Loading policy file \"{}\" into {}...",
+                                                    f, this.securityRepository);
+            try {
+                RdfUtils.upload(new File(f), this.securityRepository, null);
+            }
+            catch (IllegalArgumentException e) {
+                // Log error and continue processing policy files.
+                TechnicalException error = new TechnicalException(
+                        "policy.file.access.error", f, e.getLocalizedMessage());
+                log.error(error.getLocalizedMessage(), e);
+            }
+            catch (Exception e) {
+                // Log error and continue processing policy files.
+                TechnicalException error = new TechnicalException(
+                        "policy.file.load.error", f, e.getLocalizedMessage());
+                log.error(error.getLocalizedMessage(), e);
+            }
+        }
     }
 
     //-------------------------------------------------------------------------
