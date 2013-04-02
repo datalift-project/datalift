127183a28a326a9a3867f7a17c5e0d0aca29cbdd
Serena Villata <serena.villata@inria.fr>
2013-04-02 23:37:20 +0200

    New s4ac module fully integrated. Added authors in classes. Added s4ac target in build.xml. Updated user properties and application properties.
    Signed-off-by: Serena Villata <serena.villata@inria.fr>

--- s4ac/src/java/org/datalift/s4ac/utils/QueryType.java
+++ s4ac/src/java/org/datalift/s4ac/utils/QueryType.java
@@ -1,3 +1,17 @@
+/*
+ * Contact: serena.villata@inria.fr
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ * 
+ * @author Serena Villata (INRIA - Sophia-Antipolis)
+ * 
+ */
+
+
 package org.datalift.s4ac.utils;
 
 
