dde04b59d5fc545d1337229793f848a95d10888a
Fayçal Hamdi <fhamdi@balle.cnam.fr>
2013-06-25 20:59:48 +0200

    Delete unused files and libraries

--- geo-convert/src/java/org/datalift/geoconverter/usgs/gml2rdf/gui/package-info.java
+++
@@ -1,6 +0,0 @@
-
-/**
- * Classes for creating the GML2RDF conversion program
- * Graphical User Interface.
- */
-package org.datalift.geoconverter.usgs.gml2rdf.gui;
--- geo-convert/src/java/org/datalift/geoconverter/usgs/rdf/util/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Utility classes for working with Semantic Data.
- */
-package org.datalift.geoconverter.usgs.rdf.util;
