abf0a68a7317132bb29be1aaa9fa284f7d928e78
hedy.chain@gmail.com <hedychain@MacBook-Pro-de-hedy.local>
2013-11-12 15:48:56 +0100

    commit before integrating modele action

--- framework/src/web/js/controllers.js
+++ framework/src/web/js/controllers.js
@@ -1,6 +1,58 @@
 'use strict';
 
-function indexPageController($scope){
-    $scope.value1 = true;
-    $scope.value2 = 'COUCOU';
+var app = angular.module('project', []);
+
+/*
+app.directive('ngOntologyTitle', function(){
+	return function(scope, elem, attrs) {
+		console.log(elem);
+		console.log(attrs);
 	}
+})
+*/
+
+app.controller('projectController', function ($scope, $http){
+	
+	
+	
+	$scope.deleteSource = function(projectId, sourceUri, baseUri) 
+	{	
+		window.location = projectId + "/source/delete?uri=" + sourceUri;
+	}
+	
+	$scope.uploadOntology = function(projectId) 
+	{
+		var target = projectId + "/ontologyupload";
+		var title = ($('#ontology-title').val());
+		var source_url = ($('#ontology-cource-url').val());
+		
+		alert(target);
+		alert(title);
+		alert(source_url);
+		$.post(target, {"title": title, "source_url": source_url})
+		.done(function(data, status, xhr) {
+			window.location = xhr.getRespondHeader('Location');
+		})
+		.fail(function() {
+			alert("An error occured during the Ontology upload");
+		});
+	}
+	
+	$scope.deleteOntology = function(projectId, ontoTitle)
+	{
+		window.location = projectId + "/ontology/" + ontoTitle + "/delete";
+	}
+	
+});
+
+/*
+function ontologyController($scope, $http) {
+	
+	
+	$scope.uploadOntology = function(sourceUrl, tilte)
+	{
+		alert(sourceUrl + ' ---- ' + tilte);
+	}
+
+}
+*/
