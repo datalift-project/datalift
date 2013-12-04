b8af036d1ace3311874a3d0688742421208a9138
ArnaudValensi <arnaud.valensi@gmail.com>
2013-12-04 17:26:14 +0100

    Added some javadoc.

--- core/src/java/org/datalift/core/project/ProcessingTaskImpl.java
+++ core/src/java/org/datalift/core/project/ProcessingTaskImpl.java
@@ -1,3 +1,38 @@
+/*
+ * Copyright / Copr. 2010-2013 Atos - Public Sector France -
+ * BS & Innovation for the DataLift project,
+ * Contributor(s) : L. Bihanic, H. Devos, O. Ventura, M. Chetima
+ *                  A. Valensi
+ *
+ * Contact: dlfr-datalift@atos.net
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty and the software's author, the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading, using, modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean that it is complicated to manipulate, and that also
+ * therefore means that it is reserved for developers and experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and, more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
 package org.datalift.core.project;
 
 import javax.persistence.Entity;
@@ -8,6 +43,7 @@ import org.datalift.fwk.Configuration;
 import org.datalift.fwk.project.ProcessingTask;
 import org.datalift.fwk.project.ProjectManager;
 import org.datalift.fwk.project.TransformationModule;
+
 import com.clarkparsia.empire.annotation.NamedGraph;
 import com.clarkparsia.empire.annotation.RdfProperty;
 import com.clarkparsia.empire.annotation.RdfsClass;
@@ -19,6 +55,12 @@ import java.util.UUID;
 
 import com.google.gson.Gson;
 
+/**
+ * This class is the implementation of the interface ProcessingTask. It is an 
+ * event used to execute the task of a {@link TransformationModule}.
+ *  
+ * @author avalensi
+ */
 @Entity
 @MappedSuperclass
 @RdfsClass("datalift:TransformationEvent")
@@ -74,6 +116,11 @@ public class ProcessingTaskImpl extends EventImpl implements ProcessingTask {
     // Runnable contract implementation
     //-------------------------------------------------------------------------
 
+	/**
+	 * It is the method called by the {@link TaskManager} to execute the process
+	 * of transformation of a {@link TransformationModule}. It also update the
+	 * status and the end date of the {@link Event}.
+	 */
 	public void run() {
 		Configuration cfg = Configuration.getDefault();
 		TransformationModule m = (TransformationModule) cfg.getBean(
@@ -101,30 +148,44 @@ public class ProcessingTaskImpl extends EventImpl implements ProcessingTask {
     // Specific implementation
     //-------------------------------------------------------------------------
 
+    /** {@inheritDoc} */
+	@Override
 	public String getTransformationId() {
 		return this.transformationId;
 	}
 
+    /** {@inheritDoc} */
+	@Override
 	public EventStatus getEventStatus() {
 		return EventStatus.valueOf(this.eventStatus);
 	}
 
+    /** {@inheritDoc} */
+	@Override
 	public void setEventStatus(EventStatus eventStatus) {
 		this.eventStatus = eventStatus.toString();
 	}
 	
+    /** {@inheritDoc} */
+	@Override
 	public void addParam(String name, Object param) {
 		this.params.add(name, param);
 	}
 	
+    /** {@inheritDoc} */
+	@Override
 	public void saveParams() {
 		this.parameters = this.params.save();
 	}
 	
+    /** {@inheritDoc} */
+	@Override
 	public void loadParams() throws Exception {
 		this.params.load(this.parameters);
 	}
 	
+    /** {@inheritDoc} */
+	@Override
 	public Object getParam(String name) {
 		return this.params.getParam(name);
 	}
@@ -134,10 +195,11 @@ public class ProcessingTaskImpl extends EventImpl implements ProcessingTask {
     //-------------------------------------------------------------------------
 
 	/**
-	 * 
-	 * @author lbihanic
+	 * This class is used to serialize object to {@link String} using Gson.
+	 * <br /><br />
 	 * 
 	 * Usage:
+	 * <pre>
 	 *   JsonParam tj = new JsonParam();
 	 *   tj.add("1", "abc");
 	 *   tj.add("toto", Boolean.TRUE);
@@ -152,17 +214,42 @@ public class ProcessingTaskImpl extends EventImpl implements ProcessingTask {
 	 *   	Object o  = e.getValue();
 	 *   	System.out.println(e.getKey() + " -> " + o + " (" + o.getClass() + ')');
 	 *   }
+	 * </pre>
 	 *
+	 * @author lbihanic
 	 */
 	private class JsonParam
 	{
+
+		//---------------------------------------------------------------------
+	    // Instance members
+	    //---------------------------------------------------------------------
+		
+		/** Contain the association of key/parameter */
 		private Map<String,Object> m = new HashMap<String,Object>();
+		
+		/** Object used to serialize/de-serialize */
 		private Gson json = new Gson();
 
+		//---------------------------------------------------------------------
+	    // Methods implementation
+	    //---------------------------------------------------------------------
+		
+		/**
+		 * Add a parameter.
+		 * 
+		 * @param name is the key.
+		 * @param param is the parameter.
+		 */
 		public void add(String name, Object param) {
 			this.m.put(name, param);
 		}
 
+		/**
+		 * Save (serialize) the parameters.
+		 * 
+		 * @return The serialized {@link String}.
+		 */
 		public String save() {
 			ParamWrapper[] x = new ParamWrapper[this.m.keySet().size()];
 			int i=0;
@@ -172,6 +259,12 @@ public class ProcessingTaskImpl extends EventImpl implements ProcessingTask {
 			return json.toJson(x);
 		}
 
+		/**
+		 * Load (de-serialize) the parameters.
+		 * 
+		 * @param json is the serialized {@link String}.
+		 * @throws Exception
+		 */
 		public void load(String json) throws Exception {
 			this.m.clear();
 			ParamWrapper[] x = this.json.fromJson(json, ParamWrapper[].class);
@@ -181,10 +274,22 @@ public class ProcessingTaskImpl extends EventImpl implements ProcessingTask {
 			}
 		}
 		
+		/**
+		 * Get a parameter. load() method must be called before.
+		 * 
+		 * @param name is the key to get the parameter.
+		 * @return The parameter.
+		 */
 		public Object getParam(String name) {
 			return this.m.get(name);
 		}
 
+		/**
+		 * It is used to help the serialization.
+		 * 
+		 * @author lbihanic
+		 *
+		 */
 		private final class ParamWrapper
 		{
 			public final String name;
--- framework/src/java/org/datalift/fwk/project/ProcessingTask.java
+++ framework/src/java/org/datalift/fwk/project/ProcessingTask.java
@@ -1,7 +1,52 @@
+/*
+ * Copyright / Copr. 2010-2013 Atos - Public Sector France -
+ * BS & Innovation for the DataLift project,
+ * Contributor(s) : L. Bihanic, H. Devos, O. Ventura, M. Chetima
+ *                  A. Valensi
+ *
+ * Contact: dlfr-datalift@atos.net
+ *
+ * This software is governed by the CeCILL license under French law and
+ * abiding by the rules of distribution of free software. You can use,
+ * modify and/or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty and the software's author, the holder of the
+ * economic rights, and the successive licensors have only limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading, using, modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean that it is complicated to manipulate, and that also
+ * therefore means that it is reserved for developers and experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and, more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+
 package org.datalift.fwk.project;
 
+/**
+ * ProcessingTask is an event used to execute the task of a 
+ * {@link TransformationModule}.
+ * 
+ * @author avalensi
+ */
 public interface ProcessingTask extends Runnable, Event {
 
+    //-------------------------------------------------------------------------
+	// Enum
+    //-------------------------------------------------------------------------	
+	
 	public enum EventStatus {
 		NEW,
 		RUNNING,
@@ -9,12 +54,91 @@ public interface ProcessingTask extends Runnable, Event {
 		COMPLETE
 	}
 
+    //-------------------------------------------------------------------------
+	// ProcessingTask contract definition
+    //-------------------------------------------------------------------------
+	
+	/**
+	 * Return the id of the class which will execute the {@link ProcessingTask}.
+	 * 
+	 * @return transformationId
+	 */
 	public String getTransformationId();
+
+	/**
+	 * Get the {@link EventStatus} of the {@link ProcessingTask}.
+	 * 
+	 * @return The {@link EventStatus}.
+	 */
 	public EventStatus getEventStatus();
+
+	/**
+	 * Set the {@link EventStatus} of the {@link ProcessingTask}.
+	 * 
+	 * @param eventStatus
+	 */
 	public void setEventStatus(EventStatus eventStatus);
+
+	/**
+	 * Add a parameter into the {@link ProcessingTask} to use it in the 
+	 * execution of the {@link TransformationModule}.
+	 * saveParam() must be call to save the parameter in the 
+	 * {@link ProcessingTask}.
+	 * 
+	 * <pre>
+	 * {@code
+	 * this.addParam("param1", obj1);
+	 * this.addParam("param2", obj2);
+	 * this.saveParams();
+	 * }
+	 * </pre>
+	 * 
+	 * @param name is key of the parameter.
+	 * @param param is the parameter.
+	 */
 	public void addParam(String name, Object param);
+
+	/**
+	 * It is use to save the parameters. It must be called after calling 
+	 * addParam().
+	 * 
+	 * <pre>
+	 * {@code
+	 * this.addParam("param1", obj1);
+	 * this.addParam("param2", obj2);
+	 * this.saveParams();
+	 * }
+	 * </pre>
+	 */
 	public void saveParams();
+
+	/**
+	 * It is used to load the parameters. It must be called before getting a
+	 * param using with getParam().
+	 * 
+	 * <pre>
+	 * {@code
+	 * this.loadParams();
+	 * String  obj1 = this.getParam("param1");
+	 * Integer obj2 = this.getParam("param2");
+	 * }
+	 * </pre>
+	 */
 	public void loadParams() throws Exception;
+
+	/**
+	 * It is used to get a parameter. It must be called after loadParams().
+	 * 
+	 * <pre>
+	 * {@code
+	 * this.loadParams();
+	 * String  obj1 = this.getParam("param1");
+	 * Integer obj2 = this.getParam("param2");
+	 * }
+	 * </pre>
+	 * 
+	 * @param name is the key to get a param.
+	 */
 	public Object getParam(String name);
 
 }
