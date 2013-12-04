85f466f95da41089e012fd62bae39a1a033de954
ArnaudValensi <arnaud.valensi@gmail.com>
2013-12-04 13:32:36 +0100

    TaskManagerTest: javadoc added

--- core/tests/src/java/org/datalift/core/project/TaskManagerTest.java
+++ core/tests/src/java/org/datalift/core/project/TaskManagerTest.java
@@ -6,11 +6,10 @@ import static org.datalift.core.DefaultConfiguration.REPOSITORY_DEFAULT_FLAG;
 import static org.datalift.core.DefaultConfiguration.REPOSITORY_URIS;
 import static org.datalift.core.DefaultConfiguration.REPOSITORY_URL;
 
-import java.net.URI;
 import java.util.Properties;
 
-import org.apache.log4j.Logger;
 import org.datalift.core.DefaultConfiguration;
+import org.datalift.fwk.log.Logger;
 import org.datalift.fwk.project.ProcessingTask;
 import org.datalift.fwk.project.TaskManager;
 import org.datalift.fwk.Configuration;
@@ -25,7 +24,6 @@ import static org.junit.Assert.assertTrue;
 @RunWith(JUnit4.class)
 public class TaskManagerTest {
 	private final static String RDF_STORE = "internal";
-	private static Logger logger = Logger.getLogger(Prov.class);
 	private Properties props = new Properties();
 	private TaskManager tm = new TaskManagerImpl();
 	DefaultConfiguration cfg;
@@ -37,9 +35,8 @@ public class TaskManagerTest {
 		this.props.put(DATALIFT_HOME, "tests");
 		this.props.put(REPOSITORY_URIS, RDF_STORE);
 		this.props.put(RDF_STORE + REPOSITORY_URL, "sail:///");
-		this.props.put(RDF_STORE + REPOSITORY_URL, 
-				//"http://localhost:9091/openrdf-sesame/repositories/tests");
-				"http://localhost:9091/openrdf-sesame/repositories/internal");
+		//this.props.put(RDF_STORE + REPOSITORY_URL, 
+		//		"http://localhost:9091/openrdf-sesame/repositories/internal");
 		this.props.put(RDF_STORE + REPOSITORY_DEFAULT_FLAG, "true");
 		this.props.put(PRIVATE_STORAGE_PATH, ".");
 		
@@ -70,36 +67,29 @@ public class TaskManagerTest {
 		cfg.registerBean(m.getTransformationId(), m);
 		m.postInit(cfg);
 		m.addProcess("process1");
-//		Thread.sleep(1000);
-//		m.addProcess("process2");
-//		m.addProcess("process3");
-//		m.addProcess("process4");
-//		m.addProcess("process5");
-//		m.addProcess("process6");
 		tm.shutdown(cfg);
 		assertTrue(processExecuted);
 
 	}
 	
-	// TODO: try to automatically register the class.
 	public class TestModule extends BaseTransformationModule {
 
-	    //-------------------------------------------------------------------------
+	    //----------------------------------------------------------------------
 	    // Constants
-	    //-------------------------------------------------------------------------
+	    //----------------------------------------------------------------------
 
 	    /** The module name. */
 	    public static final String MODULE_NAME = "mymodule";
 	    
-	    //-------------------------------------------------------------------------
+	    //----------------------------------------------------------------------
 	    // Attributes
-	    //-------------------------------------------------------------------------
-
-		// private static Logger logger = Logger.getLogger(TestModule.class);
+	    //----------------------------------------------------------------------
 	    
-	    //-------------------------------------------------------------------------
+	    private Logger logger = Logger.getLogger();
+	    
+	    //----------------------------------------------------------------------
 	    // Constructors
-	    //-------------------------------------------------------------------------
+	    //----------------------------------------------------------------------
 
 	    /**
 	     * Creates a new module instance.
@@ -108,17 +98,21 @@ public class TaskManagerTest {
 	    	super(TestModule.MODULE_NAME);
 	    }
 	        
-	    //-------------------------------------------------------------------------
-	    // Web services
-	    //-------------------------------------------------------------------------
-
+	    //----------------------------------------------------------------------
+	    // Web services (in real usage)
+	    //----------------------------------------------------------------------
+	    
+	    /**
+	     * This method create a task to be given to the task manager. When a 
+	     * task is created, it is possible to save the parameters to use it in
+	     * the method which will execute the transformation.
+	     * 
+	     * @param projectId
+	     */
 	    public void addProcess(String projectId) {
 	    	ProcessingTaskImpl task = new ProcessingTaskImpl(
 	    			this.getTransformationId(),
 	    			"http://www.datalift.org/project/name/event/");
-	    	
-	    	// TODO: verify
-	    	// e.setDescription(this.getTransformationId().toString());
 	    	
 	    	task.addParam("projectId", projectId);
 	    	task.saveParams();
@@ -127,14 +121,20 @@ public class TaskManagerTest {
 	    	if (tm == null)
 	    		logger.error("TaskManager is not initialized");
 	    	tm.addTask(task);
-			logger.debug("process added");
-	    	System.out.println("[" + projectId + "] Process added.");
+	    	logger.info("Process added.");
 	    }
 
-	    //-------------------------------------------------------------------------
+	    //----------------------------------------------------------------------
 	    // TransformationModule contract
-	    //-------------------------------------------------------------------------
+	    //----------------------------------------------------------------------
 	    
+	    /**
+	     * It is the method which execute the transformation. Here it is in the
+	     * same class than the web services but it can be nice to put it in a
+	     * separate class.
+	     * 
+	     * @param task is the task made in the web service.
+	     */
 		@Override
 		public Boolean execute(ProcessingTask task) {
 			logger.debug("execute()");
@@ -147,7 +147,6 @@ public class TaskManagerTest {
 
 			logger.debug("Got param: " + projectId);
 			logger.info("Task is running...");
-	    	System.out.println("[" + projectId + "] Task is running...");
 
 			try {
 				Thread.sleep(1000);
@@ -156,8 +155,7 @@ public class TaskManagerTest {
 			}
 	    	
 			processExecuted = true;
-	    	System.out.println("[" + projectId + "] Task done.");
-			logger.info("Task done.");
+	    	logger.info("Task done.");
 			
 			return true;
 		}
