16930ddbfc3fe2083508abeca51175a39b264426
Laurent Bihanic <laurent.bihanic@atos.net>
2014-10-06 12:19:42 +0200

    Wrapper: Raise max memory to 3 Gb on Mac and Linux. Enforced bytecode validation.

--- wrapper/conf/linux/datalift.sh
+++ wrapper/conf/linux/datalift.sh
@@ -5,5 +5,6 @@ runDir=`dirname $0`
 # Start the wrapper, setting JVM library path in case some modules
 # rely on native libraries.
 java -Djava.library.path="${runDir}/bin/lib:$HOME/.datalift/lib" \
+     -Xms256M -Xmx3G -Xverify:all \
      -jar "${runDir}/bin/datalift-wrapper.jar" "$@" "${runDir}"
 #
