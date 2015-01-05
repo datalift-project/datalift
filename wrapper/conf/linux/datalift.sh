6a934a394e4d2a6e80a0e136c34160a03ab21324
Laurent Bihanic <laurent.bihanic@atos.net>
2015-01-05 17:32:24 +0100

    Wrapper: Added JVM -Djava.awt.headless=true option as Datalift is a background process. This prevents a JVM icon from appearing in the Dock or icon tray.

--- wrapper/conf/linux/datalift.sh
+++ wrapper/conf/linux/datalift.sh
@@ -6,6 +6,6 @@ runDir=`dirname $0`
 # rely on native libraries.
 java -Djava.library.path="${runDir}/bin/lib:$HOME/.datalift/lib" \
      -Xms256M -Xmx3G -Xverify:all \
-     -Djava.net.useSystemProxies=true \
+     -Djava.net.useSystemProxies=true -Djava.awt.headless=true \
      -jar "${runDir}/bin/datalift-wrapper.jar" "$@" "${runDir}"
 #
