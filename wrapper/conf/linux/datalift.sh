f0f7529aa00233f2de05710b03fa330ee45feb23
Laurent Bihanic <laurent.bihanic@atos.net>
2015-05-27 14:50:37 +0200

    Wrapper: Added comments in Datalift startup script to trace possible supplementary JVM options that can be used with Sun/Oracle JVM for various Java versions.

--- wrapper/conf/linux/datalift.sh
+++ wrapper/conf/linux/datalift.sh
@@ -9,3 +9,10 @@ java -Djava.library.path="${runDir}/bin/lib:$HOME/.datalift/lib" \
      -Djava.net.useSystemProxies=true \
      -jar "${runDir}/bin/datalift-wrapper.jar" "$@" "${runDir}"
 #
+# Supplementary options for 64bit JVMs:
+#     -XX:+UseCompressedOops \
+#
+# Supplementary options for Java 8 Update 20 and later:
+#     -XX:+UseCompressedClassPointers \
+#     -XX:+UseG1GC -XX:+UseStringDeduplication \
+#
