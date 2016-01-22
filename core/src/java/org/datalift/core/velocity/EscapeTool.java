4bd98c81b571f05733059554fc4e57bbaa6d5bd7
Laurent Bihanic <laurent.bihanic@atos.net>
2016-01-22 16:18:21 +0100

    Core: Added support for representing Json number and boolean values. Migrated to Commons Lang3 3.4 for true JSOn escaping as it differs from Javascript escaping (for example on single quote escaping).

--- core/src/java/org/datalift/core/velocity/EscapeTool.java
+++ core/src/java/org/datalift/core/velocity/EscapeTool.java
@@ -1,22 +1,32 @@
 package org.datalift.core.velocity;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 
+
+/**
+ * An extended version to Velocity EscapeTool.
+ *
+ * @author lbihanic
+ */
 public class EscapeTool extends org.apache.velocity.tools.generic.EscapeTool
 {
     public EscapeTool() {
        super();
     }
 
+    // ------------------------------------------------------------------------
+    // EscapeTool contract definition
+    // ------------------------------------------------------------------------
+
     /**
      * Escapes the characters in a <code>String</code> using
      * JavaScript String rules.
      * @param  string   the string to escape values, may be
      *                  <code>null</code>.
-     * @return String with escaped values, <code>null</code> if null
-     *         string input.
+     * @return String with escaped values, empty if null string input.
      */
     public final String js(Object string) {
-        return this.javascript(string);
+        return (string != null)? this.javascript(string): "";
     }
 
     /**
@@ -24,10 +34,24 @@ public class EscapeTool extends org.apache.velocity.tools.generic.EscapeTool
      * JavaScript String rules.
      * @param  string   the string to escape values, may be
      *                  <code>null</code>.
-     * @return String with escaped values, <code>null</code> if null
-     *         string input.
+     * @return String with escaped values, empty if null string input.
      */
     public final String json(Object string) {
-        return this.js(string);
+        return (string != null)?
+                    StringEscapeUtils.escapeJson(String.valueOf(string)): null;
+    }
+
+    public final String asJson(Object o) {
+        String json = "null";
+        if (o instanceof Number) {
+            json = String.valueOf(o);
+        }
+        else if (o instanceof Boolean) {
+            json = ((Boolean)o).toString();
+        }
+        else {
+            json = '"' + this.json(o) + '"';
+        }
+        return json;
     }
 }
